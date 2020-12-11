253b186120c187aa0030a4b274e1bf559e146b1c
AdeHikmayati <pabdillah27@gmail.com>
2020-12-12 01:38:49 +0700

    componentparts-iconText

---
+++ src/parts/iconText.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Button from "elements/Button"
+
+import React from 'react'
+
+export default function iconText() {
+    return (
+        <Button className="brand-text-icon" href="" type="link">
+          Stay<span className="text-gray-900">cation.</span>  
+       </Button>
+    )
+}
+
