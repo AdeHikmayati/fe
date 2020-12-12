c0dc27c3872e5c95ca0338594ca6bda900b4e94e
AdeHikmayati <pabdillah27@gmail.com>
2020-12-12 11:12:07 +0700

    selesai component header

--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import Button from "elements/Button"
 
-import React from 'react'
 
-export default function iconText() {
+
+export default function IconText() {
     return (
         <Button className="brand-text-icon" href="" type="link">
           Stay<span className="text-gray-900">cation.</span>  
